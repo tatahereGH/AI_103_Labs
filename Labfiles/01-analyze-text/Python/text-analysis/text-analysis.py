626459f6d79365e71ed8bbd6dab7d273baf9dafc
Fahim Khan <tatahereyahoo@gmail.com>
2026-06-23 09:02:14 -0700

    Commit with lab changes

--- Labfiles/01-analyze-text/Python/text-analysis/text-analysis.py
+++ Labfiles/01-analyze-text/Python/text-analysis/text-analysis.py
@@ -2,7 +2,9 @@ from dotenv import load_dotenv
 import os
 
 # Import namespaces
-
+# import namespaces
+from azure.identity import DefaultAzureCredential
+from azure.ai.textanalytics import TextAnalyticsClient
 
 def main():
     try:
@@ -15,7 +17,9 @@ def main():
 
 
         # Create client using endpoint
-
+        # Create client using endpoint
+        credential = DefaultAzureCredential()
+        ai_client = TextAnalyticsClient(endpoint=foundry_endpoint, credential=credential)
 
         # Analyze each text file in the reviews folder
         reviews_folder = 'reviews'
@@ -26,15 +30,29 @@ def main():
             print('\n' + text)
 
             # Get language
-
+            # Get language
+            detectedLanguage = ai_client.detect_language(documents=[text])[0]
+            print('\nLanguage: {}'.format(detectedLanguage.primary_language.name))
 
 
             # Get entities
-
+            # Get entities
+            entities = ai_client.recognize_entities(documents=[text])[0].entities
+            if len(entities) > 0:
+                print("\nEntities")
+                for entity in entities:
+                    print('\t{} ({})'.format(entity.text, entity.category))
 
 
             # Get PII
-
+            # Get PII
+            pii_result = ai_client.recognize_pii_entities(documents=[text])[0]
+            pii_entities = pii_result.entities
+            if len(pii_entities) > 0:
+                print("\nPII Entities")
+                for pii_entity in pii_entities:
+                    print('\t{} ({})'.format(pii_entity.text, pii_entity.category)) 
+                print("Redacted Text:\n {}".format(pii_result.redacted_text))
 
 
     except Exception as ex:
